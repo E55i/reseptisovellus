e16db37172a8c49a215b6c30b133b9779c7a05c7
Cerveku <art.karimaki@gmail.com>
2023-11-11 14:42:16 +0200

    Loginia
    Vielä kesken

--- screens/Login.js
+++ screens/Login.js
@@ -1,10 +1,60 @@
-import React from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, TextInput, Button, Alert } from 'react-native';
+import firebase from './FirebaseConfig'; // Oletetaan, että tämä on tiedostosi nimi
 
-export default function Login(...props) {
-    return (
-        <View>
-          <Text>Email:</Text>
-        </View>
-      )
-}
+const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        // Kirjautuminen onnistui, ohjaa käyttäjä päänäkymään
+        navigation.navigate('Main');
+      })
+      .catch(error => {
+        // Kirjautumisvirhe, näytä virheviesti
+        Alert.alert('Kirjautumisvirhe', error.message);
+      });
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Sähköposti"
+        value={email}
+        onChangeText={text => setEmail(text)}
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Salasana"
+        value={password}
+        onChangeText={text => setPassword(text)}
+        secureTextEntry
+        autoCapitalize="none"
+      />
+      <Button title="Kirjaudu sisään" onPress={handleLogin} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+  },
+  // Lisää tähän muita tyylejä tarvittaessa
+});
+
+export default LoginScreen;
