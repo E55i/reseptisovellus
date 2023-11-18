9d371a158e010ce1d827b5dae5085c908ded988f
Essi Nissinen <essieveliina.nissinen@gmail.com>
2023-11-18 14:41:15 +0200

    first version of RecipeCard

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,13 +1,90 @@
-import React from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import React from "react";
+import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
+import { Feather, MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function RecipeCard({ recipe, onSelect }) {
-    return (
-        <TouchableOpacity onPress={() => onSelect(recipe)}>
-          <View>
-            <Image source={{ uri: recipe.image }} />
-            <Text>{recipe.name}</Text>
+export default function RecipeCard({recipeId, urlToImage, recipeName, cookTime, servingSize}) {
+ 
+  return (
+    <TouchableOpacity
+      style={styles.card}
+      onPress={() => {
+        /*Show recipe, use recipeId*/
+      }}
+    >
+      {urlToImage ? (<Image style={styles.image} source={{ uri: urlToImage }} />) :  <Image style={styles.image} source={require('../assets/image_placeholder.png')} />} 
+      <View style={styles.recipeCardText}>
+        <Text style={styles.recipeName}>{recipeName}</Text>
+        <View style={styles.recipeInfo}>
+          <View style={styles.iconAndTextTime}>
+            <Feather
+              style={styles.icon}
+              name="clock"
+              size={18}
+              color="#8B8B8B"
+            />
+            <Text style={styles.infoText}>{cookTime} h</Text>
           </View>
-        </TouchableOpacity>
-      )
-    }
+          <View style={styles.iconAndTextPortion}>
+            <MaterialCommunityIcons
+              style={styles.icon}
+              name="account-group"
+              size={18}
+              color="#8B8B8B"
+            />
+            <Text style={styles.infoText}>{servingSize} hlö</Text>
+          </View>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+}
+const styles = StyleSheet.create({
+  card: {
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+    borderRadius: 10,
+    borderColor: "#47A73E",
+    borderWidth: 2,
+    padding: 8,
+    margin: 16,
+  },
+  image: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    marginRight: 36,
+    marginLeft: 2,
+  },
+  recipeCardText: {
+    flexDirection: "column",
+    flex: 1,
+    maxWidth: 200,
+    alignItems: "center",
+    flexWrap: "wrap",
+  },
+  recipeName: {
+    fontSize: 18,
+    marginTop: 4,
+    marginBottom: 12,
+  },
+  recipeInfo: {
+    flexDirection: "row",
+  },
+  iconAndTextTime: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginRight: 16,
+  },
+  iconAndTextPortion: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: 16,
+  },
+  infoText: {
+    fontSize: 12,
+  },
+  icon: {
+    paddingRight: 4,
+  },
+});
--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,43 +1,97 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
-import DefaultAppBar from '../components/DefaultAppBar'
-import Categories from '../components/Categories'
-import RecipeCard from '../components/RecipeCard'
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import React, { useEffect, useState } from "react";
+import DefaultAppBar from "../components/DefaultAppBar";
+import Categories from "../components/Categories";
+import RecipeCard from "../components/RecipeCard";
+import { getDocs } from 'firebase/firestore';
+import {
+  firestore,
+  collection
+} from "../components/FirebaseConfig";
+import ShowAlert from "../components/ShowAlert";
 
 //Hae käyttäjän nimi tietokannasta
 //Hae suosituimmat reseptit
 
 export default function Welcome({ ...props }) {
+  const [recipes, setRecipes] = useState([]);
+  const [tempRecipes, setTempRecipes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(firestore, "recipes"));
+        const recipeObject =  querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          title: doc.data().recipeData.title,
+          servingSize: doc.data().recipeData.servingSize,
+          cookTime: doc.data().recipeData.cookTime,
+          photo: doc.data().recipeData.photo,
+        }));
+
+        setRecipes(recipeObject);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        ShowAlert("Virhe","Jotain meni pieleen. Kokeile myöhemmin uudelleen.");
+      }
+    };
+
+    fetchRecipes();
+  }, [setRecipes]);
+
   return (
     <View style={styles.container}>
-      <DefaultAppBar {...props}/>
+      <DefaultAppBar {...props} />
       <Text style={styles.welcomeText}>Tervetuloa Essi</Text>
       <Text style={styles.infoText}>Mitä haluaisit kokata tänään?</Text>
-      <Categories/>
+      <Categories />
       <Text>Mainos jos kyseessä maksuton käyttäjä</Text>
-      <Text>Suosituimmat reseptit</Text>
-      <Text>RecipeCard</Text>
+      <Text style={styles.textFavourites}>Suosituimmat reseptit</Text>
+      {isLoading && (
+        <ActivityIndicator
+          style={styles.activityIndicator}
+          size="large"
+          color="#47A73E"
+        />
+      )}
+      {isLoading === false && (
+        <RecipeCard
+          recipeId={recipes[0].id}
+          urlToImage = {recipes[0].photo}
+          recipeName={recipes[0].title}
+          cookTime={recipes[0].cookTime}
+          servingSize={recipes[0].servingSize}
+        />
+      )}
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'white'
+    backgroundColor: "white",
   },
-  welcomeText:{
+  welcomeText: {
     marginLeft: 8,
     marginTop: 20,
     marginBottom: 16,
     fontSize: 16,
-    textAlign: 'center',
+    textAlign: "center",
   },
-  infoText:{
-    marginLeft: 8,
+  infoText: {
     marginTop: 8,
     marginBottom: 26,
     fontSize: 20,
-    color: 'gray',
-    textAlign: 'center',
-  }
-})
+    color: "#8B8B8B",
+    textAlign: "center",
+  },
+  textFavourites:{
+    marginTop: 8,
+    marginBottom: 8,
+    fontSize: 20,
+    color: "#8B8B8B",
+    textAlign: "center",
+  },
+});
