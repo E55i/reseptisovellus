0737f08eab5dd8d7e13d279df9f552a12432ef87
Essi Nissinen <essieveliina.nissinen@gmail.com>
2023-12-04 19:42:43 +0200

    fourth icon to appbars

--- components/DefaultAppBar.js
+++ components/DefaultAppBar.js
@@ -1,45 +1,54 @@
 import React from "react";
 import { StyleSheet } from "react-native";
 import { AppBar, HStack, IconButton } from "@react-native-material/core";
-import { FontAwesome, Ionicons } from "@expo/vector-icons";
+import { FontAwesome, Ionicons, MaterialIcons } from "@expo/vector-icons";
 
 export default function GoBackAppBar({ backgroundColor, navigation }) {
   return (
     <AppBar
       backgroundColor={backgroundColor}
-      leading={(props) => (
-        <IconButton
-          style={styles.addIcon}
-          icon={<Ionicons name="add" size={28} color="white" />}
-          onPress={() => {
-            navigation.navigate("AddRecipe");
-          }}
-          {...props}
-        />
-      )}
       title={(props) => (
         <HStack style={styles.centerContainer}>
           <IconButton
+            style={styles.icon}
+            icon={<Ionicons name="add" size={28} color="white" />}
+            onPress={() => {
+              navigation.navigate("AddRecipe");
+            }}
+            {...props}
+          />
+          <IconButton
+          style={styles.icon}
             icon={<Ionicons name="search-outline" size={28} color="white" />}
             onPress={() => navigation.navigate("SearchRecipe")}
             {...props}
           />
+           <IconButton
+           style={styles.icon}
+            icon={
+              <MaterialIcons name="my-library-books" size={28} color="white" />
+            }
+            onPress={() => navigation.navigate("OwnRecipes")}
+            {...props}
+          />
+          <IconButton
+          style={styles.icon}
+            icon={<FontAwesome name="user-circle-o" size={28} color="white" />}
+            onPress={() => navigation.navigate("Profile")}
+            {...props}
+          />
         </HStack>
-      )}
-      trailing={(props) => (
-        <IconButton
-          icon={<FontAwesome name="user-circle-o" size={28} color="white" />}
-          onPress={() => navigation.navigate("Profile")}
-          {...props}
-        />
       )}
     />
   );
 }
 const styles = StyleSheet.create({
   centerContainer: {
-    flex: 1,
     justifyContent: "center",
-    alignItems: "center",
+    alignItems: 'center',
   },
+  icon:{
+    paddingRight:'13%',
+    paddingLeft:'13%',
+  }
 });
